76765838d47674af0cd05130f3539e1d447f8ec3
Rouven Blättler <rouvenblattler@gmail.com>
2026-01-15 18:09:33 +0100

    create table/model class

--- core/pages/views.py
+++ core/pages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Note
 
 # Create your views here.
 def pages_view(request):
+    notes = Note.objects.all()
     return render(request, "pages/home.html", {
-        "name": "Django Learner" 
+        "notes": notes
     })
