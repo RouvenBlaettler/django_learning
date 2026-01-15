c20b1a2fa6815c7aa3bd1de87577c661bea4a3dc
Rouven Blättler <rouvenblattler@gmail.com>
2026-01-15 21:50:17 +0100

    create add notes feature

--- core/pages/views.py
+++ core/pages/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Note
+from .forms import NoteForm
 
 # Create your views here.
 def pages_view(request):
     notes = Note.objects.all()
+
+    if request.method == "POST":
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = NoteForm()
+
     return render(request, "pages/home.html", {
-        "notes": notes
+        "notes": notes,
+        "form" : form
     })
