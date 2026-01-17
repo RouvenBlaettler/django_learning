83b2f33592c82fa941228688c9a87324605204ea
Rouven Blättler <rouvenblattler@gmail.com>
2026-01-17 13:07:41 +0100

    add regsiter, login, logout view

--- core/pages/views.py
+++ core/pages/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Note
 from .forms import NoteForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+@login_required
 def pages_view(request):
-    notes = Note.objects.all()
+    notes = Note.objects.filter(user=request.user)
 
     if request.method == "POST":
         form = NoteForm(request.POST)
         if form.is_valid():
-            form.save()
+            note = form.save(commit=False)
+            note.user = request.user
+            note.save()
             return redirect('home')
     else:
         form = NoteForm()
@@ -19,3 +24,33 @@ def pages_view(request):
         "notes": notes,
         "form" : form
     })
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, "pages/register.html", {
+        "form": form
+    })
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, "pages/login.html")
+    return render(request, "pages/login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
