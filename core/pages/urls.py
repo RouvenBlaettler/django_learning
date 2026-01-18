6e2a88fcc6eefedb870dc4b1be148a5415759c95
Rouven Blättler <rouvenblattler@gmail.com>
2026-01-18 20:40:22 +0100

    add login and register url and template

--- core/pages/urls.py
+++ core/pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from pages.views import pages_view
+from pages.views import pages_view, register_view, login_view, logout_view
 
 urlpatterns = [
     path('', pages_view, name = "home"),
+    path("register/", register_view, name = "register"),
+    path("login/", login_view, name = "login"),
+    path("logout/", logout_view, name = "logout")
 ]
